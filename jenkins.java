80dde9727da5d0e0b9891f5d0094984e2cd55354
lakshmi4025 <88842239+lakshmi4025@users.noreply.github.com>
2021-08-17 02:49:42 -0700

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -1,6 +1,11 @@
 
-echo "welcome to the world of jenkins pipeline with java"
+ class jenkins {
 
+	public static void main(String[] args) {
+		System.out.println("Welcome to the world of jenkins pipeline with java");
 
+	}
+
+}
 
 
